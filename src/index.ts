f754d34166e094321511936da0606098b7956533
HenryDF2022 <henrydf2022@proton.me>
2022-12-27 17:35:14 +0800

    fix: export & script

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,4 @@
 export * from 'redux-saga';
 export * as effects from 'redux-saga/effects';
 
-export {
-  withLocalStore,
-  useLocalDispatch,
-} from './local-store';
+export * from './local-store';
